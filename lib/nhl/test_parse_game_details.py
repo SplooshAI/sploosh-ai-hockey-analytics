31c6b1e044a94d4b26f12c31d61a1d4a674ae2d0
Rob Brennan <rob@therobbrennan.com>
2023-10-07 22:59:16 -0700

    Add unit tests for parse_game_details

---
+++ lib/nhl/test_parse_game_details.py
@@ -0,0 +1,34 @@
+from unittest.mock import patch
+from nhl_shot_chart import parse_game_details
+
+def test_basic_game_details_extraction_home_shootout_winner():
+    NHL_GAME_ID = 2022020530  # 2022.12.22 - Seattle Kraken vs. Vancouver Canucks - Vancouver wins 6-5 in a shootout at home
+    TIMEZONE = "America/Los_Angeles"
+
+    # Live data from the NHL API
+    result = parse_game_details(NHL_GAME_ID, TIMEZONE)
+
+    # Expected values
+    GAME_START = "2022-12-22 07:00 PM PST"
+    CURRENT_PERIOD_TIME_REMAINING = "Final"
+    CURRENT_PERIOD_ORDINAL = "SO"
+
+    assert result["game"]["gameStart"] == GAME_START
+    assert result["game"]["currentPeriodTimeRemaining"] == CURRENT_PERIOD_TIME_REMAINING
+    assert result["game"]["currentPeriodOrdinal"] == CURRENT_PERIOD_ORDINAL
+
+def test_basic_game_details_extraction_visiting_shootout_winner():
+    NHL_GAME_ID = 2021020933  # 2022.03.12 - Seattle Kraken vs. Montréal Canadiens - Seattle wins 4-3 in a shootout on the road
+    TIMEZONE = "America/Los_Angeles"
+
+    # Live data from the NHL API
+    result = parse_game_details(NHL_GAME_ID, TIMEZONE)
+
+    # Expected values
+    GAME_START = "2022-03-12 04:00 PM PST"
+    CURRENT_PERIOD_TIME_REMAINING = "Final"
+    CURRENT_PERIOD_ORDINAL = "SO"
+
+    assert result["game"]["gameStart"] == GAME_START
+    assert result["game"]["currentPeriodTimeRemaining"] == CURRENT_PERIOD_TIME_REMAINING
+    assert result["game"]["currentPeriodOrdinal"] == CURRENT_PERIOD_ORDINAL
