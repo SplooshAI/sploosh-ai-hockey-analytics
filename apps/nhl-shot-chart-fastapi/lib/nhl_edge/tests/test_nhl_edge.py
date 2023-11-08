f6c043d48ee10124ca755c3a92535f220036fcd4
Rob Brennan <rob@therobbrennan.com>
2023-11-07 22:43:40 -0800

    Update tests for main and nhl_edge

--- apps/nhl-shot-chart-fastapi/lib/nhl_edge/tests/test_nhl_edge.py
+++ apps/nhl-shot-chart-fastapi/lib/nhl_edge/tests/test_nhl_edge.py
@@ -1,31 +1,58 @@
-# /lib/nhl_edge/tests/test_nhl_edge.py
+# tests/test_nhl_edge.py
 
-from fastapi.responses import HTMLResponse
+import sys
+import os
 import unittest
-from lib.nhl_edge.nhl_edge import generate_html_response
+from unittest.mock import patch, MagicMock, AsyncMock
+from fastapi.responses import HTMLResponse
+import asyncio
+
+# Ensure the parent directory is in sys.path to locate the nhl_edge module
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.insert(0, parent_dir)
+
+from nhl_edge import load_data_for_game_and_timezone
 
 class TestNHLedge(unittest.TestCase):
-    
-    def test_generate_html_response_default_timezone(self):
-        """
-        Test the generate_html_response function with default timezone.
-        """
-        gameId = "123456"
-        response = generate_html_response(gameId)
-        self.assertIsInstance(response, HTMLResponse)
-        self.assertIn("<strong>123456</strong>", response.body.decode())
-        self.assertIn("UTC", response.body.decode())
 
-    def test_generate_html_response_custom_timezone(self):
-        """
-        Test the generate_html_response function with a custom timezone.
-        """
-        gameId = "123456"
+    def setUp(self):
+        # Set up the event loop for the test case
+        self.loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(self.loop)
+
+    def tearDown(self):
+        # Close the loop after the test case
+        self.loop.close()
+
+    @patch('nhl_edge.aiohttp.ClientSession.get')
+    def test_load_data_for_game_and_timezone(self, mock_get):
+        # Mock the response from aiohttp.ClientSession.get
+        mock_response = MagicMock()
+        mock_response.__aenter__.return_value = mock_response
+        mock_response.__aexit__.return_value = None
+        mock_response.status = 200
+        # Ensure the Content-Type header is 'application/json'
+        mock_response.headers = {'Content-Type': 'application/json'}
+        # Use AsyncMock to mock the json() method
+        mock_response.json = AsyncMock(return_value={"data": "mocked data"})
+        mock_get.return_value = mock_response
+
+        # Run the coroutine inside the event loop
+        gameId = "2023020185"
         timezone = "America/Los_Angeles"
-        response = generate_html_response(gameId, timezone)
+        response = self.loop.run_until_complete(load_data_for_game_and_timezone(gameId, timezone))
+        html_content = response.body.decode()
+
+        # Check that the response is of type HTMLResponse
         self.assertIsInstance(response, HTMLResponse)
-        self.assertIn("<strong>123456</strong>", response.body.decode())
-        self.assertIn("America/Los_Angeles", response.body.decode())
+
+        # Check that the gameId appears in the response and the link is properly formatted
+        self.assertIn(gameId, html_content)
+        self.assertIn(f'href="https://www.nhl.com/gamecenter/{gameId}" target="_blank"', html_content)
+
+        # Check that the timezone appears in the response
+        self.assertIn(timezone, html_content)
 
 if __name__ == '__main__':
     unittest.main()
--- apps/nhl-shot-chart-fastapi/tests/test_main.py
+++ apps/nhl-shot-chart-fastapi/tests/test_main.py
@@ -1,52 +1,55 @@
-# tests/test_main.py
-
+import unittest
+from unittest.mock import patch
 from fastapi.testclient import TestClient
 from main import app
-import os
-import unittest
 
 client = TestClient(app)
 
 class TestMain(unittest.TestCase):
 
-    def test_get_favicon(self):
+    def test_favicon(self):
         response = client.get("/favicon.ico")
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.headers['content-type'], "image/x-icon")
+        self.assertEqual(response.headers['content-type'], 'image/x-icon')
 
-    def test_get_apple_touch_icon(self):
+    def test_apple_touch_icon(self):
         response = client.get("/apple-touch-icon.png")
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.headers['content-type'], "image/png")
+        self.assertEqual(response.headers['content-type'], 'image/png')
 
-    def test_get_apple_touch_icon_precomposed(self):
+    def test_apple_touch_icon_precomposed(self):
         response = client.get("/apple-touch-icon-precomposed.png")
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.headers['content-type'], "image/png")
+        self.assertEqual(response.headers['content-type'], 'image/png')
 
     def test_options_root(self):
         response = client.options("/")
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json(), {"allowed_methods": ["GET", "POST", "OPTIONS", "HEAD"]})
+        self.assertTrue("allowed_methods" in response.json())
 
-    def test_options_for_all_paths(self):
-        response = client.options("/some/path")
+    def test_options_all_paths(self):
+        response = client.options("/any/path")
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json(), {"allowed_methods": ["GET", "POST", "OPTIONS", "HEAD"]})
+        self.assertTrue("allowed_methods" in response.json())
 
     def test_nhl_shot_chart_get(self):
-        gameId = "test_game_id"
-        timezone = "America/Los_Angeles"
-        response = client.get(f"/?gameId={gameId}&timezone={timezone}")
-        self.assertEqual(response.status_code, 200)
-        self.assertIn(gameId, response.text)
-        self.assertIn(timezone, response.text)
+        # You'll need to mock `load_data_for_game_and_timezone` here
+        # ...
+        pass
 
     def test_nhl_shot_chart_head(self):
-        gameId = "test_game_id"
-        response = client.head(f"/?gameId={gameId}")
+        response = client.head("/", params={"gameId": "2023020185"})
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.headers['content-type'], "text/html")
+        self.assertEqual(response.headers['content-type'], 'text/html')
 
-if __name__ == '__main__':
+    # Additional tests for error cases and any other logic in your application...
+
+    @patch('main.load_data_for_game_and_timezone')
+    def test_nhl_shot_chart_get(self, mock_load_data):
+        mock_load_data.return_value = "some HTML content"
+        response = client.get("/", params={"gameId": "2023020185", "timezone": "UTC"})
+        self.assertEqual(response.status_code, 200)
+        mock_load_data.assert_called_once_with("2023020185", "UTC")
+
+if __name__ == "__main__":
     unittest.main()
