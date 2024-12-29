50fd4cf640208124f59d73ccc2f90f6d25d6b699
Rob Brennan <rob@sploosh.ai>
2024-12-29 13:37:09 -0800

    Display the Sploosh.AI NHL rink with animated Zambonis

--- apps/nhl-shot-chart/app/page.tsx
+++ apps/nhl-shot-chart/app/page.tsx
@@ -1,6 +1,7 @@
 import { Suspense } from 'react'
 import { MainLayout } from '@/components/layouts/main-layout'
 import { Version } from '@/components/version/version'
+import NHLEdgeHockeyRink from '@/components/nhl-edge-hockey-rink/nhl-edge-hockey-rink'
 
 export default function Home() {
   return (
@@ -14,7 +15,9 @@ export default function Home() {
         </div>
 
         <div className="text-center text-muted-foreground">
-          Select a game from the sidebar to view shot data
+          <div className="flex justify-center items-center w-full h-full">
+            <NHLEdgeHockeyRink className="w-full h-auto" centerIceLogo='/sploosh.ai/sploosh-ai-character-transparent.png' centerIceLogoHeight={358} centerIceLogoWidth={400} displayZamboni={true} />
+          </div>
         </div>
       </div>
     </MainLayout>
