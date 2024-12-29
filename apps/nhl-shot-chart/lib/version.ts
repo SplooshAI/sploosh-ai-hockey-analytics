e2797f5dacf8449658445efc0181ca39df11056b
Rob Brennan <rob@sploosh.ai>
2024-12-29 14:34:36 -0800

    Load appropriate values from our project root and application package.json

--- apps/nhl-shot-chart/lib/version.ts
+++ apps/nhl-shot-chart/lib/version.ts
@@ -1,7 +1,6 @@
-import packageJson from '../../../package.json';
+import rootPackageJson from '../../../package.json';
+import appPackageJson from '../package.json';
 import buildInfo from './build-info.json';
-
-const version = packageJson.version;
 
 export interface VersionInfo {
   version: string;
@@ -12,15 +11,15 @@ export interface VersionInfo {
 }
 
 export function getVersionInfo(): VersionInfo {
-  const repoUrl = packageJson.repository.url
+  const repoUrl = rootPackageJson.repository.url
     .replace('git+', '')  // Remove git+ prefix
     .replace('.git', '')  // Remove .git suffix
 
   return {
-    version: packageJson.version,
+    version: rootPackageJson.version,
     gitHash: process.env.NEXT_PUBLIC_GIT_HASH ?? buildInfo.commitHash,
     gitDate: process.env.NEXT_PUBLIC_GIT_DATE ?? buildInfo.buildTime,
-    nextJsVersion: process.env.NEXT_PUBLIC_NEXT_VERSION ?? '',
+    nextJsVersion: appPackageJson.dependencies.next ?? '',
     repoUrl
   }
 }
